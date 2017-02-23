b5a0af58731677de2c7b7ad4e207a53949f84ce2
Milagros Gonzales <milagonzales0723@gmail.com>
2017-02-23 17:44:21 -0500

    Formulario2302

--- asset/javascript.js
+++ asset/javascript.js
@@ -1,20 +1,13 @@
 function mostrardatos(){
 
-var nombre = document.getElementById("nombre");
-var apellido = document.getElementById("apellido");
-var dni = document.getElementById("dni");
-var direccion = document.getElementById("direccion");
-var inputAll = document.getElementsByName("all")
+var nombre = document.getElementById("nombre").value;
+var apellido = document.getElementById("apellido").value;
+var dni = document.getElementById("dni").value;
+var direccion = document.getElementById("direccion").value;
+var form = document.getElementById("formu");
 
-if (inputAll[0].value.length == 0 || inputAll[1].value.length == 0 || inputAll[2].value.length == 0 || inputAll[3].value.length == 0) {
-}else{
-
-	document.getElementById("form").innerHTML=
-        "<ul>" +
-          "<li>Nombre: " + nombre + "</li>" +
-          "<li>Apellido: " + apellido + "</li>" +
-          "<li>DNI: " + dni + "</li>" +
-          "<li>Dirección: " + direccion + "</li>" +
-        "</ul>";
-	}
+formu.innerHTML=( "<li>Nombre:"     + nombre      + "<br>"   +
+                      "<li>Apellidos: " + apellido    + "<br>"   +
+                      "<li>DNI: "       + dni         + "<br>"   +
+                      "<li>Direccion: " + direccion);
 }
